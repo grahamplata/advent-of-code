d04a4a296b4c5a72ac4bf17980949227b9240d89
Graham Plata <graham.plata@gmail.com>
2019-12-06 11:24:50 -0500

    Day 04

--- 2019/day04/main.py
+++ 2019/day04/main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
+
 """Day04
 Day 4 of Advent of Code
-
-It is a six-digit number.
-The value is within the range given in your puzzle input.
-Two adjacent digits are the same (like 22 in 122345).
-Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
 """
 
 import argparse
+from collections import Counter
 
 
 def runner(file_input):
@@ -17,12 +14,38 @@ def runner(file_input):
     """
 
     with open(file_input) as file:
-        line = file.readline()
-        values = line.split('-')
-        min_val, max_val = [int(i) for i in values]
+        line = file.readline().strip().split('-')
+        min_val, max_val = [int(i) for i in line]
 
-    for i in range(min_val, max_val):
-        print(i)
+    password_count_1 = 0
+    password_count_2 = 0
+
+    # The value is within the range given in your puzzle input.
+    for value in range(min_val, max_val):
+        string_val = str(value)
+
+        # length must be 6
+        if len(string_val) != 6:
+            continue
+
+        # Going from left to right, the digits never decrease;
+        if ''.join(sorted(string_val)) != string_val:
+            continue
+
+        # Two adjacent digits are the same (like 22 in 122345).
+        counter = Counter(string_val)
+        (val, count), = counter.most_common(1)
+        if count == 1:
+            continue
+        password_count_1 += 1
+
+        #  Are not part of a larger group of matching digits
+        if 2 not in counter.values():
+            continue
+        password_count_2 += 1
+
+    print(f"Part 1: {password_count_1}")
+    print(f"Part 2: {password_count_2}")
 
 
 def main():
